bd5f454919e49cc6980a7451bded5135a18bb0df
Matt Kingston <mattkingston@protonmail.com>
2026-06-11 13:52:31 +0100

    Fix widget position propagation

--- crates/eww/src/widgets/widget_definitions.rs
+++ crates/eww/src/widgets/widget_definitions.rs
@@ -796,12 +796,6 @@ fn build_gtk_scrolledwindow(bargs: &mut BuilderArgs) -> Result<gtk::ScrolledWind
     Ok(gtk_widget)
 }
 
-/// Returns `(screen_x, screen_y, width, height)` for the widget in root-window coordinates.
-fn widget_screen_geometry(widget: &impl gtk::prelude::WidgetExt) -> (i32, i32, i32, i32) {
-    let alloc = widget.allocation();
-    let origin = widget.window().map(|w| w.origin()).unwrap_or((0, 0, 0));
-    (origin.1 + alloc.x(), origin.2 + alloc.y(), alloc.width(), alloc.height())
-}
 
 const WIDGET_NAME_EVENTBOX: &str = "eventbox";
 /// @widget eventbox
@@ -844,10 +838,12 @@ fn build_gtk_event_box(bargs: &mut BuilderArgs) -> Result<gtk::EventBox> {
             connect_signal_handler!(gtk_widget, gtk_widget.connect_scroll_event(move |widget, evt| {
                 let delta = evt.delta().1;
                 if delta != 0f64 { // Ignore the first event https://bugzilla.gnome.org/show_bug.cgi?id=675959
-                    let (sx, sy, w, h) = widget_screen_geometry(widget);
+                    let sx = (evt.root().0 - evt.position().0) as i32;
+                    let sy = (evt.root().1 - evt.position().1) as i32;
+                    let alloc = widget.allocation();
                     run_command(timeout, &onscroll, &[
                         (if delta < 0f64 { "up" } else { "down" }).to_string(),
-                        sx.to_string(), sy.to_string(), w.to_string(), h.to_string(),
+                        sx.to_string(), sy.to_string(), alloc.width().to_string(), alloc.height().to_string(),
                     ]);
                 }
                 glib::Propagation::Proceed
@@ -859,10 +855,12 @@ fn build_gtk_event_box(bargs: &mut BuilderArgs) -> Result<gtk::EventBox> {
             gtk_widget.add_events(gdk::EventMask::ENTER_NOTIFY_MASK);
             connect_signal_handler!(gtk_widget, gtk_widget.connect_enter_notify_event(move |widget, evt| {
                 if evt.detail() != NotifyType::Inferior {
-                    let (sx, sy, w, h) = widget_screen_geometry(widget);
+                    let sx = evt.root().0 - evt.position().0;
+                    let sy = evt.root().1 - evt.position().1;
+                    let alloc = widget.allocation();
                     run_command(timeout, &onhover, &[
                         evt.position().0, evt.position().1,
-                        sx as f64, sy as f64, w as f64, h as f64,
+                        sx, sy, alloc.width() as f64, alloc.height() as f64,
                     ]);
                 }
                 glib::Propagation::Proceed
@@ -874,10 +872,12 @@ fn build_gtk_event_box(bargs: &mut BuilderArgs) -> Result<gtk::EventBox> {
             gtk_widget.add_events(gdk::EventMask::LEAVE_NOTIFY_MASK);
             connect_signal_handler!(gtk_widget, gtk_widget.connect_leave_notify_event(move |widget, evt| {
                 if evt.detail() != NotifyType::Inferior {
-                    let (sx, sy, w, h) = widget_screen_geometry(widget);
+                    let sx = evt.root().0 - evt.position().0;
+                    let sy = evt.root().1 - evt.position().1;
+                    let alloc = widget.allocation();
                     run_command(timeout, &onhoverlost, &[
                         evt.position().0, evt.position().1,
-                        sx as f64, sy as f64, w as f64, h as f64,
+                        sx, sy, alloc.width() as f64, alloc.height() as f64,
                     ]);
                 }
                 glib::Propagation::Proceed
@@ -966,11 +966,13 @@ fn build_gtk_event_box(bargs: &mut BuilderArgs) -> Result<gtk::EventBox> {
         ) {
             gtk_widget.add_events(gdk::EventMask::BUTTON_PRESS_MASK);
             connect_signal_handler!(gtk_widget, gtk_widget.connect_button_release_event(move |widget, evt| {
-                let (sx, sy, w, h) = widget_screen_geometry(widget);
+                let sx = (evt.root().0 - evt.position().0) as i32;
+                let sy = (evt.root().1 - evt.position().1) as i32;
+                let alloc = widget.allocation();
                 match evt.button() {
-                    1 => run_command(timeout, &onclick, &[sx, sy, w, h]),
-                    2 => run_command(timeout, &onmiddleclick, &[sx, sy, w, h]),
-                    3 => run_command(timeout, &onrightclick, &[sx, sy, w, h]),
+                    1 => run_command(timeout, &onclick, &[sx, sy, alloc.width(), alloc.height()]),
+                    2 => run_command(timeout, &onmiddleclick, &[sx, sy, alloc.width(), alloc.height()]),
+                    3 => run_command(timeout, &onrightclick, &[sx, sy, alloc.width(), alloc.height()]),
                     _ => {},
                 }
                 glib::Propagation::Proceed
